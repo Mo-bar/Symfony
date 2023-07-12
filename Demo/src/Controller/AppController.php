12a182c81edeef6b673847c9d1945a07516e09f4
mourad_br <mouradbarkouch@gmail.com>
2023-07-12 16:21:45 +0100

    Ajouter le chemin de la page  au  via l'annotation

--- Demo/src/Controller/AppController.php
+++ Demo/src/Controller/AppController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AppController
 {
+    /**
+     * @Route("/")
+     * @return Response
+     */
  public function index(){
-     return new Response("Welcome in Symfony");
+     return new Response("Welcome in Symfony_");
  }
 }
