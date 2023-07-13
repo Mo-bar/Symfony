a6422269131871fa1a88b0dfca3baa6c7eca65cd
mourad_br <mouradbarkouch@gmail.com>
2023-07-13 16:15:41 +0100

    cree les pages dauth & routage

--- Demo/src/Controller/AppController.php
+++ Demo/src/Controller/AppController.php
@@ -10,11 +10,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     /**
-     * @Route("/{name}")
+     * @Route("/home/{name}")
      * @return Response
      */
  public function index(string $name)
  {
     return $this-> render('home.html.twig', ['name' => $name]);
  }
+
+    /**
+     * @Route("/signup",name="signup-pg")
+     * @return Response
+     */
+ public function signup(){
+     return $this->render('auth/signup.html.twig');
+ }
+
+    /**
+     * @Route("/login",name="login-pg")
+     * @return Response
+     */
+public function login(){
+    return $this-> render('auth/login.html.twig');
+}
+
 }
